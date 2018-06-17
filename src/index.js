b20549b12493a96ee4c2af92d45dc5c48b9db43e
gadij00@gmail.com <gadij00@gmail.com>
2018-06-17 17:26:30 +0300

    render first react element

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Hello world");
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+    return (
+        <div>render my first element</div>
+    );
+}
+
+ReactDOM.render(<App/>, document.getElementById("root"));
